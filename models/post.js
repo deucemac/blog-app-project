ef40d93a20d441ffd8c516b6c18ceac3d764fbbf
Shean <csheanjohnson@gmail.com>
2020-06-15 15:25:45 -0400

    updated schema to match seed file structure

--- models/post.js
+++ models/post.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema
 
 const Post = new Schema(
   {
-    title: { type: String, required: true },
+    name: { type: String, required: true },
     imgURL: { type: String, required: true },
-    content: { type: String, required: true },
-    author: {type: String, required: true}
+    description: { type: String, required: true },
+    user: {type: String, required: true}
   },
   {timestamps: true}
 )
