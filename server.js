25fe4893112a600df99bfb109b577673e8eb47f0
Dwayne McDowell <dwaynemcdowell@dwaynes-mbp.home>
2020-06-15 19:13:51 -0400

    resolved-troubleshoot

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(logger('dev'))
 
-app.use('/api', postsRoutes)
+app.use('/', postsRoutes)
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
