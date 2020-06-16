07edf153f35acd40bc8fc704cc1401e332fd3fb4
Shean <csheanjohnson@gmail.com>
2020-06-16 13:53:11 -0400

    changed function names

--- client/src/components/Sort.js
+++ client/src/components/Sort.js
@@ -9,7 +9,7 @@ const compareKey = key =>
     return 0
   }
 
-export const AZ = arr => arr.sort(compareKey('name'))
-export const ZA = arr => arr.sort(compareKey('name')).reverse()
-export const lowerstFirst = arr => arr.sort((a, b) => +a.price - +b.price)
-export const highestFirst = arr => arr.sort((a, b) => +b.price - +a.price)
+export const alph = arr => arr.sort(compareKey('name'))
+export const revAlph = arr => arr.sort(compareKey('name')).reverse()
+export const lowPrice = arr => arr.sort((a, b) => +a.price - +b.price)
+export const highPrice = arr => arr.sort((a, b) => +b.price - +a.price)
